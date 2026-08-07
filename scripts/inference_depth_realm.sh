687449d16827c6e8f085bc0a71100ca01990c142
marc haubenstock <marc.dhaubenstock@gmail.com>
2026-08-07 13:06:15 +0000

    removing output dir

--- scripts/inference_depth_realm.sh
+++ scripts/inference_depth_realm.sh
@@ -11,17 +11,23 @@ echo "Running depth inference on custom_images dataset: $REALM_DATASET"
 IMAGE_SHAPE=$(python3 -c "import yaml; s=yaml.safe_load(open('config/dataset/custom_images.yaml'))['image_shape']; print(f'{s[0]},{s[1]}')")
 echo "Using image_shape: [$IMAGE_SHAPE]"
 
+OUTPUT_DIR=outputs/depthsplat-depth-base-$REALM_DATASET
+if [ -d "$OUTPUT_DIR" ]; then
+    echo "Removing existing output directory: $OUTPUT_DIR"
+    rm -rf "$OUTPUT_DIR"
+fi
+
 CUDA_VISIBLE_DEVICES=0 python -m src.main \
 +experiment=re10k \
 dataset=custom_images \
 dataset.roots=[datasets/custom_images] \
 dataset.scene_selection=$REALM_DATASET \
 dataset.test_chunk_interval=1 \
-dataset/view_sampler=arbitrary \
+dataset/view_sampler=evaluation \
+dataset.view_sampler.num_context_views=5 \
+dataset.view_sampler.index_path=assets/evaluation_index_realm.json \
 mode=test \
 dataset.image_shape=[$IMAGE_SHAPE] \
-dataset.view_sampler.num_context_views=2 \
-dataset.view_sampler.num_target_views=10 \
 model.encoder.num_scales=2 \
 model.encoder.upsample_factor=4 \
 model.encoder.lowest_feature_resolution=8 \
@@ -32,7 +38,7 @@ test.compute_scores=false \
 test.save_depth=true \
 test.save_depth_concat_img=true \
 test.save_depth_npy=true \
-output_dir=outputs/depthsplat-depth-base-$REALM_DATASET
+output_dir=$OUTPUT_DIR
 
 
 
